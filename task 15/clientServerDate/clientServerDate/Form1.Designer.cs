fee1b8cb1de66d6550a09e45b954520fd20abd54
slsAMG <slsAMG2011@yandex.ru>
2019-01-20 16:32:38 +0300

    temp impl task 15 with client server has error

---
+++ task 15/clientServerDate/clientServerDate/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace clientServerDate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start server ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "send query client";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(382, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "query: getDate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ task 15/clientServerDate/clientServerDate/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clientServerDate
+{
+    public partial class Form1 : Form
+    {
+        private bool serverStart = false;
+        private TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 9999);
+        Thread t1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!serverStart)
+            {
+                serverStart = true;
+                listener.Start();
+                t1 = new Thread((new ThreadStart(() =>
+                {
+                    while (true)
+                    {
+
+                        TcpClient client = listener.AcceptTcpClient();
+                        NetworkStream stream = client.GetStream();
+                        byte[] buffer = new byte[client.ReceiveBufferSize];
+                        int bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
+                       
+                        string dataRecieved = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                        string textToSend;
+                        if (dataRecieved.Equals("getDate"))
+                        {
+                            textToSend = DateTime.Now.ToString();
+                        }
+                        else {
+                            textToSend = "incorrect query";
+                        }
+                        byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+                        stream.Write(bytesToSend, 0, bytesToSend.Length);
+                        
+                       
+                    }
+                })));
+
+                button1.Text = "stop server";
+                t1.Start();
+               
+            }
+            else
+            {
+
+                button1.Text = "start server";
+                serverStart = false;
+                listener.Stop();
+                t1.Suspend();
+            }
+            
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                TcpClient client = new TcpClient("127.0.0.1", 9999);
+                NetworkStream nwStream = client.GetStream();
+                string sendMessage;
+                if (textBox1.Text.Equals(""))
+                {
+                    sendMessage = "incorrect";
+
+                }
+                else
+                {
+                    sendMessage = textBox1.Text;
+                }
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(sendMessage);
+
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+                byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                textBox1.Text=Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+            
+            }
+            catch
+            {
+                MessageBox.Show("connection failed");
+            }
+        }
+    }
+}
