75064e633a10622aa3edb2469d72f541f4c49b57
slsAMG <slsAMG2011@yandex.ru>
2019-01-27 15:52:14 +0300

    add new tasks with transfer files with tcp/ip

---
+++ task 12/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"),9999);
+        bool startServer = false;
+        Thread t1;
+        string path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //server
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (startServer)
+            {
+                listener.Stop();
+                t1.Abort();
+                button1.Text = "start server";
+            }
+            else
+            {
+                t1 = new Thread(new ThreadStart(() =>
+                {
+                    while (true)
+                    {
+                        TcpClient client = listener.AcceptTcpClient();
+                        NetworkStream stream = client.GetStream();
+                        byte[] buffer = new byte[client.ReceiveBufferSize];
+                        int size = stream.Read(buffer, 0, buffer.Length);
+                        string inputString = Encoding.ASCII.GetString(buffer, 0, size);
+
+                        string path = @inputString;
+                        byte[] fileBytes = File.ReadAllBytes(path);
+                        stream.Write(fileBytes, 0, fileBytes.Length);
+                    }
+                }));
+                button1.Text = "stop server";
+                listener.Start();
+                startServer = true;
+                t1.Start();
+            }
+
+        }
+        //browse to html file
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.ShowDialog();
+            path = fd.InitialDirectory + fd.FileName;
+        }
+        //client
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!path.Equals("") || path.EndsWith(".html"))
+            {
+                try
+                {
+                    TcpClient client = new TcpClient("127.0.0.1", 9999);
+                    NetworkStream stream = client.GetStream();
+                    byte[] bytesToSend = Encoding.ASCII.GetBytes(path);
+                    stream.Write(bytesToSend,0,bytesToSend.Length);
+                    byte[] buffer = new byte[client.ReceiveBufferSize];
+                    int size = stream.Read(buffer, 0, buffer.Length);
+                    FolderBrowserDialog fbd = new FolderBrowserDialog();
+                    fbd.ShowDialog();
+                    string folderPath = fbd.SelectedPath;
+                    Random random = new Random();
+                    folderPath +="\\"+random.Next()+".html";
+                    File.Create(folderPath).Dispose();
+                    File.WriteAllBytes(folderPath, buffer);
+
+                    System.Diagnostics.Process.Start(folderPath);
+                    client.Close(); 
+                }
+                catch {
+                    MessageBox.Show("client error");
+                }
+            }
+            else
+            {
+
+                MessageBox.Show("file is not html");
+
+
+            }
+            
+
+        }
+    }
+}
---
+++ task 20/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Thread t1;
+        bool start = false;
+        TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"),9999);
+        string path = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!path.Equals("") && path.EndsWith("txt"))
+            {
+                try
+                {
+                    TcpClient client = new TcpClient("127.0.0.1", 9999);
+                    NetworkStream stream = client.GetStream();
+
+                    byte[] bytesToSend = Encoding.ASCII.GetBytes(path);
+                    stream.Write(bytesToSend, 0, bytesToSend.Length);
+                    byte[] buffer = new byte[client.ReceiveBufferSize];
+                    int bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
+                    FolderBrowserDialog fbd = new FolderBrowserDialog();
+                    fbd.ShowDialog();
+                    Random r = new Random();
+                    string pathToSaveFile = fbd.SelectedPath+"\\"+r.Next()+".txt";
+                    File.Create(path).Dispose();
+
+                    File.WriteAllBytes(pathToSaveFile, buffer);
+                    richTextBox1.Text =Encoding.ASCII.GetString(buffer);
+                    
+                  
+                }
+                catch
+                {
+                    MessageBox.Show("client error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("empty file or not txt");
+             
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            
+                path = ofd.InitialDirectory + ofd.FileName;
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!start)
+            {
+                button1.Text = "stop server";
+                t1 = new Thread(new ThreadStart(() =>
+                {
+                    while (true)
+                    {
+                        TcpClient client = listener.AcceptTcpClient();
+                        NetworkStream stream = client.GetStream();
+                        byte[] recievedBytes = new byte[client.ReceiveBufferSize];
+                        int bytesRead = stream.Read(recievedBytes, 0, recievedBytes.Length);
+                        string recievedPath = Encoding.ASCII.GetString(recievedBytes, 0, bytesRead);
+                        string path = @recievedPath;
+                        byte[] fileBytes = File.ReadAllBytes(path);
+                        stream.Write(fileBytes, 0, fileBytes.Length);
+                    }
+                }));
+                listener.Start();
+                t1.Start();
+            }
+            else
+            {
+                start = false;
+                button1.Text = "start server";
+            }
+        }
+    }
+}
