8d42ef4cca0be918332f0ca6285eb34861e9a7c3
slsAMG <slsAMG2011@yandex.ru>
2019-01-28 22:25:12 +0300

    add odbc connection

---
+++ task 18/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=E:\\angular\\home.accdb;";
+            OdbcConnection conn = new OdbcConnection(connectionString);
+            string myQuery = "Select home, room from homes";
+            conn.Open();
+            OdbcCommand command = new OdbcCommand(myQuery);
+            
+            command.Connection = new OdbcConnection(connectionString);
+      
+            OdbcDataAdapter adapter = new OdbcDataAdapter();
+            adapter.SelectCommand = command;
+            DataSet myDataSet = new DataSet();
+       
+            string dataTable = "tableA";
+            adapter.Fill(myDataSet, dataTable);
+            DataTable myDataTable = myDataSet.Tables[dataTable];
+             foreach (DataRow rd in myDataTable.Rows)
+            {
+                Console.WriteLine(rd["room"]);
+                Console.WriteLine(rd["home"]);
+
+            }
+Console.ReadLine();
+
+        }
+    }
+}
